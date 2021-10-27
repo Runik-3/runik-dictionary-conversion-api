9833dd5224b305a3144585fd921d41fdb700f51d
matteomiceli <omatteomiceli@gmail.com>
2021-10-27 09:26:41 -0700

    setup environmental variable for debug mode - finalized dependencies with pipenv

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask
 from flask import request
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 app = Flask(__name__)
 
 @app.route('/', methods=["POST", "GET"])
 def test(): 
-    return 
+    if request.method == "GET":
+        print('hi')
+    return 'hello'
+
+
+
+if __name__ == '__main__':
+    app.run('localhost', 8080)
