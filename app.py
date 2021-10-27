e492c912d1e230689d1fed01ac91ef6e91a0d9f9
Matteo Miceli <omatteomiceli@gmail.com>
2021-10-26 18:21:46 -0700

    added generate dict function

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-from flask import Request
+from flask import request
 
 
 app = Flask(__name__)
 
-@app.route('/', methods=["POST"])
+@app.route('/', methods=["POST", "GET"])
 def test(): 
+    if request.method=="GET":
+        print ('get')
     return '<h1>test</h1>'
---
+++ controllers/generate_dictionary.py
@@ -0,0 +1,3 @@
+
+def generate_dictionary(target, input_file):
+    # code goes here
