3bc95bf6ebfea0624cc6e15647e44db35769acfe
matteomiceli <omatteomiceli@gmail.com>
2021-10-27 12:03:52 -0700

    ai url takes in target device and dictionary title

--- routes/api.py
+++ routes/api.py
@@ -3,6 +3,6 @@ from flask import Blueprint
 api = Blueprint('api', __name__)
 
 
-@api.route('/<test>')
-def index(test):
-    return (test)
+@api.route('/<device>/<title>', methods=["POST"])
+def index(device, title):
+    return (f'{device} {title}')
