4c8c1c8b2cb34544b276321058ed8386b2875d9a
Matteo Miceli <omatteomiceli@gmail.com>
2021-10-31 16:35:45 -0700

    getting ready for production - installed gunicorn WSGI server - setup heroku procfile

--- app/helpers/cleanup_helper.py
+++ app/helpers/cleanup_helper.py
@@ -1,10 +1,11 @@
-from os import path
+import os
 from shutil import rmtree
 
-output_dir = path.join(path.dirname(__file__), '..',
+output_dir = os.path.join(os.path.dirname(__file__), '..',
                         '..', 'dictionaries', 'output')
 
 # cleans up the output folder
 def cleanup(response):
     rmtree(output_dir)
+    os.mkdir(output_dir)
     return response
