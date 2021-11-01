248869f52306a0a550906c794e2a7651d181638a
Matteo Miceli <omatteomiceli@gmail.com>
2021-11-01 13:38:24 -0700

    committing dictionary dir with cleanup to remove and add back input

--- app/helpers/cleanup_helper.py
+++ app/helpers/cleanup_helper.py
@@ -1,11 +1,17 @@
 import os
 from shutil import rmtree
 
+input_path = os.path.join(os.path.dirname(__file__), '..',
+                        '..', 'dictionaries', 'input')
+
 output_dir = os.path.join(os.path.dirname(__file__), '..',
                         '..', 'dictionaries', 'output')
 
 # cleans up the output folder
 def cleanup(response):
+    rmtree(input_path)
     rmtree(output_dir)
+    os.mkdir(input_path)
     os.mkdir(output_dir)
+
     return response
