a72716a522e5337fc1b317d8ebdb4e10c86d4295
Matteo Miceli <omatteomiceli@gmail.com>
2021-11-01 15:32:30 -0700

    squelched warnings, only printing errors from pyglossary

--- app/helpers/converters/convert_dictionary_helper.py
+++ app/helpers/converters/convert_dictionary_helper.py
@@ -14,7 +14,7 @@ def convert_dictionary(dictionary_title, input_format):
     output_path = path.join(path.dirname(__file__), '..',
                        '..', '..','dictionaries', 'output', f'dicthtml-{dictionary_title}-en.kobo.zip')
 
-    cmd_string = f'pyglossary {input_path} {output_path} --read-format={input_format} --write-format=Kobo'
+    cmd_string = f'pyglossary -v1 {input_path} {output_path} --read-format={input_format} --write-format=Kobo'
     cmd_array = cmd_string.split()
 
     subprocess.run(cmd_array)
